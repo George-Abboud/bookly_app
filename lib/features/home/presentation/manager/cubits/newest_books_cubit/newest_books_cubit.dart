23d0a49f20e723c397ab0eea73a65eb25d3594d4
George-Abboud <geo1111ang@gmail.com>
2024-10-19 20:58:40 +0300

    create fetch newest book function

--- lib/features/home/presentation/manager/cubits/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/cubits/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,31 @@
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var resault = await homeRepo.fetchFeaturedBooks();
+    resault.fold(
+      (failure) {
+        emit(
+          NewestBooksFailure(
+            errorMessage: failure.errMessage,
+          ),
+        );
+      },
+      (books) {
+        emit(
+          NewestBooksSuccess(
+            books: books,
+          ),
+        );
+      },
+    );
+  }
 }
